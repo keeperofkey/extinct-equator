11028941e6ecd151d81fd4e60e653c956f29ddcb
keeperofkey <doddtastic@gmail.com>
2023-05-08 13:59:41 -0400

    three js gltf boiler plate

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-	experimental: {
-		assets: true
-	},
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  experimental: {
+    assets: true
+  },
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), svelte()]
 });
